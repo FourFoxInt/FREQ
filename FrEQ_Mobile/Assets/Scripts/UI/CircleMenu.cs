3389d775d58bc79ade1dd01b406dc3caa9a83e94
jrsimmondsnz <fourfoxinteractive@gmail.com>
2023-01-17 13:41:27 +1300

    circle menus fix

--- FrEQ_Mobile/Assets/Scripts/UI/CircleMenu.cs
+++ FrEQ_Mobile/Assets/Scripts/UI/CircleMenu.cs
@@ -5,21 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class CircleMenu : MonoBehaviour
 {
-    [SerializeField] private Animator bkgAni;
     [SerializeField] private Animator menuCircleAni;
     [SerializeField] private Animator profileCircleAni;
     [SerializeField] private Animator menuBtnAni;
     [SerializeField] private Animator profileBtnAni;
 
-    [SerializeField] private GameObject bkg;
-
     private bool menuIn = false;
     private bool profileMenuIn = false;
 
-    private void Start()
-    {
-        bkg.SetActive(false);
-    }
     public void Menu_BtnClick()
     {
         if (!profileMenuIn)
@@ -27,19 +20,12 @@ public class CircleMenu : MonoBehaviour
             if (menuIn)
             {
                 menuIn = false;
-                StartCoroutine(bkgOut());
                 menuCircleAni.SetBool("in", false);
-                menuBtnAni.SetBool("in", false);
-                profileBtnAni.SetBool("menuIn", false);
             }
             else if (!menuIn)
             {
                 menuIn = true;
-                bkg.SetActive(true);
-                bkgAni.SetBool("in", true);
                 menuCircleAni.SetBool("in", true);
-                menuBtnAni.SetBool("in", true); 
-                profileBtnAni.SetBool("menuIn", true);
             }
         }
     }
@@ -50,25 +36,14 @@ public class CircleMenu : MonoBehaviour
             if (profileMenuIn)
             {
                 profileMenuIn = false;
-                StartCoroutine(bkgOut());
                 profileCircleAni.SetBool("in", false);
-                profileBtnAni.SetBool("in", false);
             }
             else if (!profileMenuIn)
             {
                 profileMenuIn = true;
-                bkg.SetActive(true);
-                bkgAni.SetBool("in", true);
                 profileCircleAni.SetBool("in", true);
-                profileBtnAni.SetBool("in", true);
             }
         }
-    }
-    IEnumerator bkgOut()
-    {
-        bkgAni.SetBool("in", false);
-        yield return new WaitForSeconds(1);
-        bkg.SetActive(false);
     }
 
     //MENU BTNS
